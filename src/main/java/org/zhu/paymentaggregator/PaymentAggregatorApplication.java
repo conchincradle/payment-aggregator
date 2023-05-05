9f22d65767548a62e8059e8ff82620624c07639c
conchincradle <33627638+conchincradle@users.noreply.github.com>
2023-05-05 23:11:34 +0900

    Add files via upload

---
+++ src/main/java/org/zhu/paymentaggregator/PaymentAggregatorApplication.java
@@ -0,0 +1,13 @@
+package org.zhu.paymentaggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentAggregatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentAggregatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/zhu/paymentaggregator/PaymentAggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.zhu.paymentaggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentAggregatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
